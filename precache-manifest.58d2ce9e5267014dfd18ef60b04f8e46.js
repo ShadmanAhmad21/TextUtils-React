f886d500facad709ee5bb93908b6263a3325a884
Shadman <s.shadmanahmad2138@gmail.com>
2023-04-14 14:45:26 +0530

    Updates

---
+++ precache-manifest.58d2ce9e5267014dfd18ef60b04f8e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1f58069999ecbd978f6",
+    "url": "/TextUtils-React/static/css/main.d65228e6.chunk.css"
+  },
+  {
+    "revision": "e1f58069999ecbd978f6",
+    "url": "/TextUtils-React/static/js/main.e1f58069.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtils-React/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "f910467d99116a391852",
+    "url": "/TextUtils-React/static/js/2.f910467d.chunk.js"
+  },
+  {
+    "revision": "d59b444719da3c63ba0b",
+    "url": "/TextUtils-React/static/js/runtime~main.d59b4447.js"
+  },
+  {
+    "revision": "3b4308f003c25e6ecb7ee0dd343c6397",
+    "url": "/TextUtils-React/index.html"
+  }
+];
